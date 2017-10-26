dc88b365e8210405fabafbd9b341fe2f614eb333
Harald Wartig <hwartig@gmail.com>
2017-10-26 21:14:27 +0200

    Make spec pass

--- spec/xgboost_spec.rb
+++ spec/xgboost_spec.rb
@@ -1,11 +1,5 @@
-require "spec_helper"
-
 RSpec.describe Xgboost do
   it "has a version number" do
-    expect(Xgboost::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+    expect(Xgboost::VERSION).to eq('0.1.0')
   end
 end
