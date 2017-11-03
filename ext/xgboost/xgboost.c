637b8592316a0b23fb53f5b9302cc7a3e1be441f
Harald Wartig <hwartig@gmail.com>
2017-11-03 20:26:10 +0100

    Remove Exploratory Test and c code

--- ext/xgboost/xgboost.c
+++ ext/xgboost/xgboost.c
@@ -2,30 +2,8 @@
 
 VALUE rb_mXgboost;
 
-VALUE rb_xgboost_this_is_c(VALUE klass)
-{
-  VALUE r_result = rb_int_new(42);
-  return r_result;
-}
-
-VALUE rb_xgboost_add_in_c(VALUE klass, VALUE op1, VALUE op2)
-{
-  if (!FIXNUM_P(op1))
-    rb_raise(rb_eArgError, "Argument 1 has to be of type Fixnum");
-
-  if (!FIXNUM_P(op2))
-    rb_raise(rb_eArgError, "Argument 2 has to be of type Fixnum");
-
-  long op1_long = FIX2LONG(op1);
-  long op2_long = FIX2LONG(op2);
-
-  return LONG2FIX(op1_long + op2_long);
-}
-
 void
 Init_xgboost(void)
 {
   rb_mXgboost = rb_define_module("Xgboost");
-  rb_define_module_function(rb_mXgboost, "this_is_c", rb_xgboost_this_is_c, 0);
-  rb_define_module_function(rb_mXgboost, "add_in_c", rb_xgboost_add_in_c, 2);
 }
--- spec/xgboost_spec.rb
+++ spec/xgboost_spec.rb
@@ -2,21 +2,4 @@ RSpec.describe Xgboost do
   it "has a version number" do
     expect(Xgboost::VERSION).to eq('0.1.0')
   end
-
-  it "can talk to C" do
-    expect(Xgboost.this_is_c).to eq(42)
-  end
-
-  it "can add two numbers in C" do
-    expect(Xgboost.add_in_c(20, 22)).to eq(42)
-  end
-
-  it "can add other two numbers in C" do
-    expect(Xgboost.add_in_c(1, 2)).to eq(3)
-  end
-
-  it "only accepts numeric inputs" do
-    expect { Xgboost.add_in_c("foo", 2) }.to raise_error(ArgumentError, "Argument 1 has to be of type Fixnum")
-    expect { Xgboost.add_in_c(2, nil) }.to raise_error(ArgumentError, "Argument 2 has to be of type Fixnum")
-  end
 end
