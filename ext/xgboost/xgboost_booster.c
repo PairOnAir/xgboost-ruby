9af8feba51f9ad743f5260c5033a547c54d44367
Harald Wartig <hwartig@gmail.com>
2017-11-10 21:01:59 +0100

    Implement Booster#predict.

--- ext/xgboost/xgboost_booster.c
+++ ext/xgboost/xgboost_booster.c
@@ -2,12 +2,6 @@
 
 VALUE rb_cXgboostBooster;
 
-static void
-rb_xgboost_booster_deallocate(BoosterHandle bh)
-{
-  XGBoosterFree(bh);
-}
-
 static VALUE
 rb_xgboost_booster_allocate(VALUE klass)
 {
@@ -17,18 +11,10 @@ rb_xgboost_booster_allocate(VALUE klass)
   return Data_Wrap_Struct(klass, NULL, rb_xgboost_booster_deallocate, bh);
 }
 
-static VALUE
-rb_xgboost_booster_save(VALUE self, VALUE file_name)
+static void
+rb_xgboost_booster_deallocate(BoosterHandle bh)
 {
-  BoosterHandle bh;
-
-  Check_Type(file_name, T_STRING);
-
-  Data_Get_Struct(self, BoosterHandle, bh);
-
-  XGBoosterSaveModel(bh, StringValuePtr(file_name));
-
-  return Qnil;
+  XGBoosterFree(bh);
 }
 
 static VALUE
@@ -45,11 +31,56 @@ rb_xgboost_booster_load(VALUE self, VALUE file_name)
   return Qnil;
 }
 
+static VALUE
+rb_xgboost_booster_predict(VALUE self, VALUE matrix)
+{
+  BoosterHandle bh;
+
+  Check_Type(matrix, T_ARRAY);
+
+  Data_Get_Struct(self, BoosterHandle, bh);
+  long ncols = RARRAY_LEN(matrix);
+
+  float *c_matrix = calloc(ncols, sizeof(float));
+
+  for (long i = 0; i < ncols; i++)
+    c_matrix[i] = NUM2DBL(RARRAY_AREF(matrix, i));
+
+  DMatrixHandle dmatrix;
+
+  XGDMatrixCreateFromMat(c_matrix, 1, ncols, 0, &dmatrix);
+
+  const float *preds;
+  unsigned long long pred_len;
+
+  XGBoosterPredict(bh, dmatrix, 0, 0, &pred_len, &preds);
+
+  XGDMatrixFree(dmatrix);
+  free(c_matrix);
+
+  return DBL2NUM(preds[0]);
+}
+
+static VALUE
+rb_xgboost_booster_save(VALUE self, VALUE file_name)
+{
+  BoosterHandle bh;
+
+  Check_Type(file_name, T_STRING);
+
+  Data_Get_Struct(self, BoosterHandle, bh);
+
+  XGBoosterSaveModel(bh, StringValuePtr(file_name));
+
+  return Qnil;
+}
+
 void
 Init_xgboost_booster(void)
 {
   rb_cXgboostBooster = rb_define_class_under(rb_mXgboost, "Booster", rb_cObject);
   rb_define_alloc_func(rb_cXgboostBooster, rb_xgboost_booster_allocate);
-  rb_define_method(rb_cXgboostBooster, "save", rb_xgboost_booster_save, 1);
   rb_define_method(rb_cXgboostBooster, "load", rb_xgboost_booster_load, 1);
+  rb_define_method(rb_cXgboostBooster, "predict", rb_xgboost_booster_predict, 1);
+  rb_define_method(rb_cXgboostBooster, "save", rb_xgboost_booster_save, 1);
 }
--- spec/xgboost/booster_spec.rb
+++ spec/xgboost/booster_spec.rb
@@ -8,4 +8,18 @@ RSpec.describe Xgboost::Booster do
 
     expect { copy.load(file.path) }.not_to raise_error
   end
+
+  describe '#predict' do
+    context 'when passed a one dimensional array' do
+      let(:booster) do
+        booster = Xgboost::Booster.new
+        booster.load(File.join(File.dirname(__FILE__), '../fixtures/linear.model'))
+        booster
+      end
+
+      it 'returns a single prediction for that one example' do
+        expect(booster.predict([2.0])).to be_within(0.00001).of(4.03468)
+      end
+    end
+  end
 end
