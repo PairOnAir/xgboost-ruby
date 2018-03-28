b44b8a4d234d04bbf1f20f74c634ceea43c3d57e
Harald Wartig <hwartig@gmail.com>
2018-03-28 11:55:53 +0200

    Make `gem install xgboost` work on mac.

--- ext/xgboost/extconf.rb
+++ ext/xgboost/extconf.rb
@@ -1,13 +1,32 @@
 require 'mkmf'
 
+def say_and_run(cmd)
+  puts "About to run: #{cmd}"
+  `#{cmd}`
+end
+
+target = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'vendor', 'xgboost'))
+
+FileUtils.mkdir_p(target)
+
+# clone xgboost
+say_and_run("git clone --recursive --jobs 4 --depth 1 git@github.com:dmlc/xgboost.git #{target}")
+say_and_run("cd #{target}; git submodule update --remote")
+
+# run make in xgboost
+say_and_run("cd #{target}; cp make/minimum.mk config.mk; make -j 4")
+
+# run install name tool to fix library reference
+say_and_run("cd #{target}; install_name_tool -id '#{target}/lib/libxgboost.dylib' lib/libxgboost.dylib")
+
 HEADER_DIRS = [
- '../../../../../xgboost/include',
- '../../../../../xgboost/rabit/include',
+  File.join(target, 'include'),
+  File.join(target, 'rabit', 'include'),
 ]
 
 LIB_DIRS = [
-  '../../../../../xgboost/lib',
-  '../../../../../xgboost/rabit/lib',
+  File.join(target, 'lib'),
+  File.join(target, 'rabit', 'lib'),
 ]
 
 dir_config('xgboost', HEADER_DIRS, LIB_DIRS)
