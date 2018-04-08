c909c80aa08bf199440dc792c3727c2cbd853996
Olek Janiszewski <olek.janiszewski@gmail.com>
2018-04-08 12:16:08 +0200

    Symlink shared library to /usr/local/lib

--- bin/install_xgboost.sh
+++ bin/install_xgboost.sh
@@ -3,6 +3,11 @@
 set -e
 set -xv
 
+if ls /usr/local/lib/libxgboost.* > /dev/null 2>&1; then
+  echo Shared library already installed in /usr/local/lib
+  exit
+fi
+
 target="$(dirname $0)/../vendor/xgboost"
 mkdir -p "$target"
 target=$(cd "$target"; pwd -P)
@@ -19,4 +24,16 @@ if [[ "$OSTYPE" == darwin* ]]; then
   cp make/minimum.mk config.mk
 fi
 
-make -j 4
+make --jobs 4
+
+if [ -f lib/libxgboost.so ]; then
+  ext=so
+elif [ -f lib/libxgboost.dylib ]; then
+  ext=dylib
+fi
+
+if [ -n "$ext" ]; then
+  ln -s "$target/lib/libxgboost.$ext" /usr/local/lib
+else
+  echo "Didn't find any shared library in $target/lib to symlink into /usr/local/lib"
+fi
